c07433bfcebb833f55b600e91caeda2972ae3e58
rladmstn <oz33177351@gmail.com>
2024-11-19 18:23:29 +0900

    feat(view) : 입출력 관련 메세지 상수화 및 기본 view 구현

---
+++ src/main/java/baseball/view/OutputView.java
@@ -0,0 +1,10 @@
+package baseball.view;
+
+import baseball.constant.PrintMessage;
+
+public class OutputView {
+    public void printGameStart(){
+        System.out.println(PrintMessage.OUTPUT_GAME_START);
+    }
+
+}
